e3913f6071e53309dc9c1cfe62374e6a6a13dd36
danielshahanski <daniel_sh.99@abv.bg>
2017-03-31 22:38:22 +0300

    hw

--- lesson6/homework/Daniel-toto/script.js
+++ lesson6/homework/Daniel-toto/script.js
@@ -1,3 +1,36 @@
 /**
  * Created by User on 29.3.2017.
  */
+document.addEventListener("DOMContentLoaded", load);
+
+var numbers=new Array;
+var elements;
+
+function load(){
+    document.getElementById('button').addEventListener('click', fillArr)
+    elements = document.querySelectorAll(".number");
+}
+function drawNum() {
+    return Math.floor(Math.random()*49) + 1;
+}
+
+function fillArr(){
+    while(numbers.length<6){
+        var num = drawNum();
+        var exists = 0;
+        for (var i = numbers.length - 1; i >= 0; i--) {
+            if(numbers[i] == num) exists = 1;
+        }
+        if(exists == 0) numbers[numbers.length] = num;
+    }
+    displayArr();
+}
+var br=0;
+function displayArr(){
+    if(br < elements.length){
+        elements[br].innerHTML = numbers[br];
+        elements[br].style.backgroundColor="#CABE8A";
+        br++;
+        setTimeout(displayArr, 1000);
+    }
+}
